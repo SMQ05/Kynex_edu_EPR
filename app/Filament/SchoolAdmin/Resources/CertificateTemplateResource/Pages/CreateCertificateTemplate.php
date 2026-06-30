56c3f072b9ba87889530a42c412969a512f4956f
SMQ05 <smyasir1512@gmail.com>
2026-06-30 11:55:51 -0700

    fix(certicate and id): index error

--- app/Filament/SchoolAdmin/Resources/CertificateTemplateResource/Pages/CreateCertificateTemplate.php
+++ app/Filament/SchoolAdmin/Resources/CertificateTemplateResource/Pages/CreateCertificateTemplate.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateCertificateTemplate extends CreateRecord
 {
     protected static string $resource = CertificateTemplateResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return static::getResource()::getUrl('index');
+    }
 }
